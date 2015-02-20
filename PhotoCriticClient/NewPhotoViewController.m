d516fd025bae5dc9ec8011ed8eacee5d9667c8b9
Travis Luong <travis@travisluong.com>
2015-02-20 15:30:50 -0800

    take photo and push to rails api

--- PhotoCriticClient/NewPhotoViewController.m
+++ PhotoCriticClient/NewPhotoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NewPhotoViewController.h"
+#import "AppDelegate.h"
 
 @interface NewPhotoViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
@@ -81,6 +82,91 @@
     
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (IBAction)submit:(id)sender {
+    AppDelegate *app = [UIApplication sharedApplication].delegate;
+//
+//    NSData *imageData = UIImagePNGRepresentation(self.imageView.image);
+//    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[imageData length]];
+//    
+//    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+//    [request setHTTPMethod:@"POST"];
+//    
+//    NSString *requestString = [NSString stringWithFormat:@"http://localhost:3000/api/v1/photos?user_email=%@&user_token=%@", [app.authInfo objectForKey:@"email"], [app.authInfo objectForKey:@"authentication_token"]];
+//    NSURL *url = [NSURL URLWithString:requestString];
+//
+//    [request setURL:url];
+//    
+//    [request setValue:@"multipart/form-data" forHTTPHeaderField:@"Content-Type"];
+//    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+//    [request setHTTPBody:imageData];
+    
+    // Dictionary that holds post parameters. You can set your post parameters that your server accepts or programmed to accept.
+    NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    [_params setObject:@"1.0" forKey:@"ver"];
+    [_params setObject:@"en" forKey:@"lan"];
+    [_params setObject:@"new image" forKey:@"title"];
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *BoundaryConstant = @"----------V2ymHFg03ehbqgZCaKO6jy";
+    
+    // string constant for the post parameter 'file'. My server uses this name: `file`. Your's may differ
+    NSString* FileParamConstant = @"photo[pic]";
+    
+    // the server url to which the image (or the media) is uploaded. Use your server url here
+    NSString *requestString = [NSString stringWithFormat:@"http://localhost:3000/api/v1/photos?user_email=%@&user_token=%@", [app.authInfo objectForKey:@"email"], [app.authInfo objectForKey:@"authentication_token"]];
+    NSURL *requestURL = [NSURL URLWithString:requestString];
+
+    // create request
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30];
+    [request setHTTPMethod:@"POST"];
+    
+    // set Content-Type in HTTP header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", BoundaryConstant];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    // post body
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    // add image data
+    NSData *imageData = UIImagePNGRepresentation(self.imageView.image);
+    if (imageData) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"image.png\"\r\n", FileParamConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString:@"Content-Type: image/png\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:imageData];
+        [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // setting the body of the post to the reqeust
+    [request setHTTPBody:body];
+    
+    // set the content-length
+    NSString *postLength = [NSString stringWithFormat:@"%d", [body length]];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    
+    // set URL
+    [request setURL:requestURL];
+    
+    NSURLSessionDataTask *dataTask = [app.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"%@", data);
+    }];
+    
+    [dataTask resume];
+}
+
 
 /*
 #pragma mark - Navigation
